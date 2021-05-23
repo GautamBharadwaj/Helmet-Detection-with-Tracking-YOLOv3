8bce05f56a1e1362b91bdfec6769bf0f284eebc3
GautamBharadwaj <80876143+GautamBharadwaj@users.noreply.github.com>
2021-05-23 12:57:11 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -6,13 +6,18 @@ from deep_sort import nn_matching
 from deep_sort.tracker import Tracker
 import imutils
 from gautam_draw import draw_bounding_boxes
-from gautam_video_write import save_into_video
 from object_detection import YOLO
 
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+frame_width = 1200
+frame_height = 675https://github.com/GautamBharadwaj/Helmet-Detection-with-Tracking-YOLOv3/blob/main/predict.py
+out = cv2.VideoWriter('C:/Users/Gautam/Desktop/final_folder/output/output_file2.avi', fourcc, 20.0,(frame_width, frame_height))
+
 #################### Loading the Weights and configuration file into our system ##################
 
-path = "C:/Users/Gautam/Desktop/final_folder/helmet.mp4"
+path = "C:/Users/Gautam/Desktop/final_folder/helmet1.mp4"
+video = cv2.VideoCapture(path)
 modelpath = 'C:/Users/Gautam/Desktop/final_folder/'
 net = cv2.dnn.readNet('{}/yolov3-helmet.weights'.format(modelpath), '{}/yolov3-helmet.cfg'.format(modelpath))
 classes = []
@@ -51,7 +56,7 @@ while True:
     draw_bounding_boxes(tracker,detections,frame)
 
     frame = imutils.resize(frame, width=1200)
-    save_into_video(path,frame)
+    out.write(frame)
     cv2.imshow('Image', frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
